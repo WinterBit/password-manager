6612180613aacfce1ef23ae4ce22a8cf08030d8c
WinterBit <royhemant990@gmail.com>
2025-04-13 17:38:44 +0530

    Designed Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,11 +3,12 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-      <nav className='w-full h-12 border-2 flex justify-between border-red-500'>
-        <div className="icon">
-            <span>Pass</span><span>Manage</span>
+      <nav className='w-full py-3 flex justify-between border-b-[0.5px] border-gray-500'>
+        <div className="icon ml-16 p-2 text-2xl">
+            <span className='text-purple-500'>&lt; </span><span>Pass</span><span className='text-purple-500'>Manage</span> <span className='text-purple-500'> /</span><span className='text-purple-500'> &gt;</span>
         </div>
-        <div className="login">
+
+        <div className="login p-2 px-6 mr-16 text-xl cursor-pointer gradient-btn">
             LogIn
         </div>
       </nav>
